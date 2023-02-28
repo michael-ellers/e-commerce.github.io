cceb9dd883aba17a95947828ee9b230ccf9f0bbc
Michael Ellers <michael.ellers@stapleacquisitions.com>
2023-02-28 17:32:53 -0500

    fifth adjustment to root index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './client/index.css';
-import App from './client/src/App';
+import './index.css';
+import App from './App';
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-import { theme } from "./client/src/theme";
+import { theme } from "./theme";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./client/src/state";
+import cartReducer from "./state";
 
 const store = configureStore({
     reducer: { cart: cartReducer }
